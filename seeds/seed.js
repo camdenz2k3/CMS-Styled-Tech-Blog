1167a3bf3559cc9ece9f7394a3712b2acfa43fb6
Snow Cactus <camdenz2k3@gmail.com>
2023-02-05 19:09:52 -0600

    created blogdata and added to seed.js to include blog data

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,6 +2,7 @@ const sequelize = require('../config/connection')
 const {User, Blog} = require('../models')
 
 const userData = require('./userData.json')
+const blogData = require('./blogData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({force: true})
@@ -10,6 +11,13 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true
   })
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id
+    })
+  }
 
   process.exit(0)
 }
