956c74c0ce7f3e4662f914a16fa788c8ce36b6b2
Snow Cactus <camdenz2k3@gmail.com>
2023-02-05 19:02:15 -0600

    created the create.js for creating a blog

---
+++ public/js/create.js
@@ -0,0 +1,30 @@
+const createBlog = document.getElementById('createBlog')
+
+// creates new blog
+createBlog.addEventListener('submit', (event) => {
+  event.preventDefault()
+  
+  const {
+    title: titleInput,
+    content: contentInput
+  } = event.target.elements
+
+  const blogInfo = {
+    title: titleInput.value,
+    content: contentInput.value
+  }
+  
+  fetch('/api/blogs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(blogInfo)
+  })
+  .then(response => {
+    if (response.status === 200) {
+      window.location.href = '/dashboard'
+    }
+  })
+  .catch(err => console.log(err))
+})
