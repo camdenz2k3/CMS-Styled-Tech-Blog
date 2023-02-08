b4138ab4f4fcad23be2226485ba93a19df5d0473
Snow Cactus <camdenz2k3@gmail.com>
2023-02-07 18:42:04 -0600

    fixed sign up problem and adjusted colors

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,16 @@
 const loginFormHandler = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });const loginFormHandler = async (event) => {
-        event.preventDefault();
-      
-        const email = document.querySelector('#email-login').value.trim();
-        const password = document.querySelector('#password-login').value.trim();
-      
-        if (email && password) {
-          const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
-          });
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
       
           if (response.ok) {
             window.location.href = "/";
@@ -84,17 +71,8 @@ const loginFormHandler = async (event) => {
         document
           .querySelector(".signup-form")
           .addEventListener("submit", signupFormHandler);
-        
   
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
-    }
-  };
   
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+        document
+          .querySelector('.login-form')
+          .addEventListener('submit', loginFormHandler);
